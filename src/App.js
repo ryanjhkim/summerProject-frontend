393eb248332a54353453a11a73b99fd3bf3b7cad
JH Kim <ehwo78@gmail.com>
2021-06-14 11:43:57 +0900

    React router set-up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-function App() {
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+
+function App () {
   return (
-    <div>Hello world!</div>
+    <Router>
+      <Switch>
+        <Route exact path="/"><Home/></Route>
+        <Route path="/about"><About/></Route>
+      </Switch>
+    </Router>
   );
 }
 
